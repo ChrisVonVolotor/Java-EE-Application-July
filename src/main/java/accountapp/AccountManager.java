b28f62ef927641af83dd9a3fa665c08f162ac32b
ChrisVonVolotor <evilchris9@gmail.com>
2018-07-12 11:53:15 +0100

    Relationship for Many to One

---
+++ src/main/java/accountapp/AccountManager.java
@@ -0,0 +1,27 @@
+package accountapp;
+
+import javax.persistence.*;
+
+public class AccountManager {
+	/*
+		 EntityManagerFactory emf =
+			      Persistence.createEntityManagerFactory("myDbFile.odb");
+		 
+		  EntityManager em = emf.createEntityManager();
+		  try {
+			  em.getTransaction().begin();
+			  Account account1 = new Account(1234L,"Jane","Doe");
+			  Account account2 = new Account(1235L,"John","Doe");
+			  Account account3 = new Account(1236L,"Jim","Taylor");
+			  em.persist(account1);
+			  em.persist(account2);
+			  em.persist(account3);
+	
+		  }
+		  finally {
+		      em.close();
+		  }
+	  */
+	  
+
+}
